aeb8e9984ec92de06b65e6486dd6697e52a3d049
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 18:52:14 -0800

    moved to bot framework

--- example_bot.py
+++ example_bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from discord.ext import commands
 
 client = discord.Client()
 
@@ -7,12 +8,16 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+@bot.command
+async def hello(ctx):
+    await ctx.send("Hello "+ctx.author)
 
 client.run(os.environ['BOT_TOKEN'])
