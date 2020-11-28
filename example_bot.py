fdac8947ea8aaceb9cdf7fedb134f41c9087199b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 19:24:34 -0800

    bot.run now included

--- example_bot.py
+++ example_bot.py
@@ -1,20 +1,9 @@
 import discord
 import os
 from discord.ext import commands
+import logging
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='$')
 
@@ -27,4 +16,4 @@ async def hello(ctx, foo):
     await ctx.send("Hello "+ctx.author)
 
 
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
