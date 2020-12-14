a6664296bddde33573c6dadc69e8da24910f22df
Esme <emeralddinogirl@gmail'com>
2020-12-13 20:03:52 -0800

    added hungry command

--- example_bot.py
+++ example_bot.py
@@ -3,17 +3,26 @@ import os
 from discord.ext import commands
 import logging
 
+from discord.ext.commands import Bot
+
 logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix='$')
+bot: Bot = commands.Bot(command_prefix='$')
+
 
 @bot.command()
 async def empty(ctx):
     await ctx.send("empty")
 
+
 @bot.command()
 async def hello(ctx, foo):
     await ctx.send("Hello "+ctx.author)
+
+
+@bot.command()
+async def hungry(ctx):
+    await ctx.send("food")
 
 
 bot.run(os.environ['BOT_TOKEN'])
