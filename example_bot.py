4455d06486b5e39b29ad187f5db01da04b5077a4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-13 21:14:08 -0800

    testing plot parsing

--- example_bot.py
+++ example_bot.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 import logging
+import re
 
 from discord.ext.commands import Bot
 
@@ -24,5 +25,22 @@ async def hello(ctx, foo):
 async def hungry(ctx):
     await ctx.send("food")
 
+
+# plot from X to Y
+plotRegex = re.compile("^from (\\d) ?, ?(\\d) ?to ?(\\d) ?, ?(\\d)$")
+
+
+@bot.command()
+async def plot(ctx, *, args):
+    match = plotRegex.match(args)
+    if not match:
+        await ctx.send("Invalid plot command.  Ex: 'plot from 25,6 to 32,9'")
+    else:
+        from_x = match.group(1)
+        from_y = match.group(2)
+        to_x = match.group(3)
+        to_y = match.group(4)
+
+        await ctx.send(f'I should probably do math on going from {from_x},{from_y} over to {to_x},{to_y}')
 
 bot.run(os.environ['BOT_TOKEN'])
